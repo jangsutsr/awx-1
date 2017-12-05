d463d6a0eea8f09e538da8632002c3f703d7e4b1
Jake McDermott <jmcdermott@ansible.com>
2017-12-05 00:10:56 -0500

    add WorkflowJobTemplate model

--- awx/ui/client/lib/models/index.js
+++ awx/ui/client/lib/models/index.js
@@ -4,16 +4,16 @@ import Base from '~models/Base';
 import Config from '~models/Config';
 import Credential from '~models/Credential';
 import CredentialType from '~models/CredentialType';
-import Me from '~models/Me';
-import Organization from '~models/Organization';
-import Project from '~models/Project';
-import JobTemplate from '~models/JobTemplate';
-import WorkflowJobTemplateNode from '~models/WorkflowJobTemplateNode';
-import InventorySource from '~models/InventorySource';
 import Inventory from '~models/Inventory';
 import InventoryScript from '~models/InventoryScript';
-
+import InventorySource from '~models/InventorySource';
+import JobTemplate from '~models/JobTemplate';
+import Me from '~models/Me';
 import ModelsStrings from '~models/models.strings';
+import Organization from '~models/Organization';
+import Project from '~models/Project';
+import WorkflowJobTemplate from '~models/WorkflowJobTemplate';
+import WorkflowJobTemplateNode from '~models/WorkflowJobTemplateNode';
 
 const MODULE_NAME = 'at.lib.models';
 
@@ -25,14 +25,15 @@ angular
     .service('ConfigModel', Config)
     .service('CredentialModel', Credential)
     .service('CredentialTypeModel', CredentialType)
-    .service('MeModel', Me)
-    .service('OrganizationModel', Organization)
-    .service('ProjectModel', Project)
-    .service('JobTemplateModel', JobTemplate)
-    .service('WorkflowJobTemplateNodeModel', WorkflowJobTemplateNode)
-    .service('InventorySourceModel', InventorySource)
     .service('InventoryModel', Inventory)
     .service('InventoryScriptModel', InventoryScript)
-    .service('ModelsStrings', ModelsStrings);
+    .service('InventorySourceModel', InventorySource)
+    .service('JobTemplateModel', JobTemplate)
+    .service('MeModel', Me)
+    .service('ModelsStrings', ModelsStrings)
+    .service('OrganizationModel', Organization)
+    .service('ProjectModel', Project)
+    .service('WorkflowJobTemplateModel', WorkflowJobTemplate)
+    .service('WorkflowJobTemplateNodeModel', WorkflowJobTemplateNode);
 
 export default MODULE_NAME;
